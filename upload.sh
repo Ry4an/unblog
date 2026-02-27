aad6166f3bc4a82456b1262e147e132a441b3f57
Ry4an Brase <ry4an-githubpublic@ry4an.org>
2026-02-27 09:09:53 -0500

    Only upload clean builds.

--- upload.sh
+++ upload.sh
@@ -2,4 +2,9 @@
 
 echo "Don't forget to mfa first!"
 
+if grep -rq localhost:1313 public ; then
+    echo Do a clean build first!
+    exit 1
+fi
+
 aws s3 sync --delete --size-only "$@" public s3://ry4an.org-origin/unblog
